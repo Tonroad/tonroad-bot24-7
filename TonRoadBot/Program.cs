156fdc39867ac350a177f5b360600d4f96bf7cb8
Tonroad <tonroad.map@web.de>
2025-07-07 09:52:26 +0200

    first bot version

---
+++ TonRoadBot/Program.cs
@@ -0,0 +1,52 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+var botClient = new TelegramBotClient("7740992334:AAHS2q_ogUV7YW1jPg3b5z9FjLtf6fOojwU");
+
+using var cts = new CancellationTokenSource();
+
+botClient.StartReceiving(
+	HandleUpdateAsync,
+	HandleErrorAsync,
+	new ReceiverOptions { AllowedUpdates = { } },
+	cancellationToken: cts.Token
+);
+
+Console.WriteLine("Bot started. Press any key to exit.");
+Console.ReadKey();
+
+async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
+{
+	if (update.Message is { } message && message.Text != null)
+	{
+		if (message.Text == "/start")
+		{
+			var webAppInfo = new WebAppInfo
+			{
+				Url = "https://tonroad-map.vercel.app"
+			};
+
+			var keyboard = new InlineKeyboardMarkup(new[]
+			{
+				InlineKeyboardButton.WithWebApp("🌍 Open TonRoad Map", webAppInfo)
+			});
+
+			await bot.SendPhotoAsync(
+				chatId: message.Chat.Id,
+				photo: InputFile.FromUri("https://raw.githubusercontent.com/tonroad/tonroad-map/main/tonroad_logo.jpg"),
+				caption: "Добро пожаловать в TonRoad!\nНажмите кнопку ниже, чтобы открыть карту.",
+				replyMarkup: keyboard,
+				cancellationToken: cancellationToken
+			);
+		}
+	}
+}
+
+Task HandleErrorAsync(ITelegramBotClient bot, Exception exception, CancellationToken cancellationToken)
+{
+	Console.WriteLine(exception.Message);
+	return Task.CompletedTask;
+}
+
